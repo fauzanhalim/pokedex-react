a09852df6b851444999ac626dbd8ed89a3084516
fauzanhalim <nurfauzanhalim@gmail.com>
2023-11-03 05:58:57 +0700

    update unit testing

--- src/app.test.jsx
+++ src/app.test.jsx
@@ -9,4 +9,20 @@ test('renders app with title', () => {
     expect(titleElement).toBeInTheDocument();
 });
 
+test('displays "Pokemon not found" for invalid search', async () => {
+    render(<App />);
+    const searchInput = screen.getByPlaceholderText('Search your pokemon ... 😏');
+    fireEvent.change(searchInput, { target: { value: 'InvalidPokemonName' } });
+    fireEvent.click(screen.getByText('Search'));
+});
+
+test('performs search functionality', async () => {
+    render(<App />);
+    const searchInput = screen.getByPlaceholderText('Search your pokemon ... 😏');
+    fireEvent.change(searchInput, { target: { value: 'Pikachu' } });
+    fireEvent.click(screen.getByText('Search'));
+
+    // Check if the "Load More" button is not visible
+    expect(screen.queryByText('Load More')).toBeNull();
+});
 
